40d281b2c6f4e609d503d0b24cd5ff998f46d9d3
Ryan Darley <spook6@gmail.com>
2015-01-14 21:27:12 -0600

    Forgot to commit the control script
    Also removing Character Controller, we can add it back later if needed

---
+++ Assets/Scripts/BallControl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallControl : MonoBehaviour {
+
+	int rotationSpeed = 200;
+	int jumpHeight = 8;
+
+	float distToGround;
+	// Use this for initialization
+	void Start () 
+	{
+		distToGround = collider.bounds.extents.y;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+		rotation *= Time.deltaTime;
+		rigidbody.AddRelativeTorque(Vector3.back * rotation);
+
+		if(Input.GetButtonDown("Jump") /*Input.GetKeyDown(KeyCode.Space)*/ && IsGrounded())
+		{
+			rigidbody.velocity = new Vector3(0, jumpHeight, 0);
+		}	
+	}
+
+	bool IsGrounded()
+	{
+		//Check if we are on the ground. Return true if we are else return false.
+		float rayDistance = distToGround + (float)0.1;
+		return Physics.Raycast(transform.position, Vector3.down, rayDistance);
+	}
+}
